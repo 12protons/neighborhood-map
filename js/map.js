9369ef66eeac70d082c81ed1c7c9bfd0e1eebf90
Mark Gatesman <mark.j.gatesman@gmail.com>
2016-05-22 16:44:53 -0400

    Selected marker animates

--- js/map.js
+++ js/map.js
@@ -1,11 +1,16 @@
+var markers = [];
+var selectedMarker;
+var map;
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 42.373, lng: -71.100},
       zoom: 15
     });
 
 
     var locations = (new LocationsViewModel).locations();
+    var infowindow = new google.maps.InfoWindow();
 
     for (i = 0; i < locations.length; i++) {  
         marker = new google.maps.Marker({
@@ -13,5 +18,19 @@ function initMap() {
             map: map,
             title: locations[i].name
         });
+
+        markers.push(marker);
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                if (marker == selectedMarker) {
+                    return;
+                } else if (selectedMarker != null) {
+                    selectedMarker.setAnimation(null);
+                }
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                selectedMarker = marker;
+            }
+        })(marker, i));
     }
 }
